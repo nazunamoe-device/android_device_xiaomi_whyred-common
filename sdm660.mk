ecd0bfa8ab476d95279c3079b19c4cc39a5941cb
nazunamoe <nakashimayuki@kakao.com>
2022-09-25 23:10:48 +0900

    sdm660-common: Change to lineage overlay

--- sdm660.mk
+++ sdm660.mk
@@ -7,7 +7,7 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-superior
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
